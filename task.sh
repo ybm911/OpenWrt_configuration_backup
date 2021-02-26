bb726c2b2e4d881b36429e4ae934cccfdb1840b6
Elli0t <50075517+ybm911@users.noreply.github.com>
2021-02-26 20:51:33 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-RUN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+RUN_DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )";
 CMD="echo $RUN_DIR | sh $RUN_DIR/run.sh"
 echo "0 7 */2 * *   root    $CMD" > /etc/cron.d/OpenWrt_configuration_backup;
 /etc/init.d/cron reload;
